759e6bb9844649032b80b1573e2ffdffda96286d
sf-95 <sfarch95@gmail.com>
2024-11-17 15:17:53 +0000

    Removed link from H1 and add to individual timezone selections

--- javascript/index.js
+++ javascript/index.js
@@ -44,7 +44,9 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )} <small>${cityTime.format("A")} </small></div>
-      </div>`;
+      </div>
+      <a href="index.html">All cities</a>
+      `;
 }
 updateTime();
 setInterval(updateTime, 1000);
